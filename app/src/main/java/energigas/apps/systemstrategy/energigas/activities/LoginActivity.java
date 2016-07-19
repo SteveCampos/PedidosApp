0f7fe6b1572da0ce66cd1d501fbd534b78593a64
Steve Campos Vega <js7campos@gmail.com>
2016-07-19 12:18:00 -0500

    Login View

---
+++ app/src/main/java/energigas/apps/systemstrategy/energigas/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package energigas.apps.systemstrategy.energigas.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import energigas.apps.systemstrategy.energigas.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
