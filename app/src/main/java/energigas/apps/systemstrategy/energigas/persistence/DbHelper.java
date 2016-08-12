3a9162a19d4cc47d876c387b221dfc8fd58c16c9
kelvinthony@gmail.com <kelvin-+ja4ever>
2016-08-12 17:26:43 -0500

    importacion de accesos

--- app/src/main/java/energigas/apps/systemstrategy/energigas/persistence/DbHelper.java
+++ app/src/main/java/energigas/apps/systemstrategy/energigas/persistence/DbHelper.java
@@ -21,10 +21,26 @@ public class DbHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
+        sqLiteDatabase.execSQL(DB_Usuario.CREATE_TABLA_DB_USUARIO);
+        sqLiteDatabase.execSQL(DB_Rol.CREATE_TABLA_DB_ROL);
+        sqLiteDatabase.execSQL(DB_Acceso.CREATE_TABLA_DB_ACCESO);
+        sqLiteDatabase.execSQL(DB_Privilegio.CREATE_TABLA_DB_PRIVILEGIO);
+        sqLiteDatabase.execSQL(DB_RolAcceso.CREATE_TABLA_DB_ROLACCESO);
+        sqLiteDatabase.execSQL(DB_RolUsuario.CREATE_TABLA_DB_ROLUSUARIO);
+        sqLiteDatabase.execSQL(DB_RolPrivilegio.CREATE_TABLA_DB_ROLPRIVILEGIO);
+
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        sqLiteDatabase.execSQL(DB_Usuario.DELETE_TABLA_DB_USUARIO);
+        sqLiteDatabase.execSQL(DB_Rol.DELETE_TABLA_DB_ROL);
+        sqLiteDatabase.execSQL(DB_Acceso.DELETE_TABLA_DB_ACCESO);
+        sqLiteDatabase.execSQL(DB_Privilegio.DELETE_TABLA_DB_PRIVILEGIO);
+        sqLiteDatabase.execSQL(DB_RolAcceso.DELETE_TABLA_DB_ROLACCESO);
+        sqLiteDatabase.execSQL(DB_RolUsuario.DELETE_TABLA_DB_ROLUSUARIO);
+        sqLiteDatabase.execSQL(DB_RolPrivilegio.DELETE_TABLA_DB_ROLPRIVILEGIO);
+        onCreate(sqLiteDatabase);
     }
 }
